7b0f855e41e23b5f19baec33dcd0d249aacbb419
Abraham De Alba <abrahamdealba@abrahams-macbook-pro.local>
2018-10-10 14:17:46 -0700

    Initial Commit

---
+++ Tumblrapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tumblrapp
+//
+//  Created by Abraham De Alba on 10/10/18.
+//  Copyright © 2018 Abraham De Alba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ TumblrappTests/TumblrappTests.swift
@@ -0,0 +1,34 @@
+//
+//  TumblrappTests.swift
+//  TumblrappTests
+//
+//  Created by Abraham De Alba on 10/10/18.
+//  Copyright © 2018 Abraham De Alba. All rights reserved.
+//
+
+import XCTest
+@testable import Tumblrapp
+
+class TumblrappTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
